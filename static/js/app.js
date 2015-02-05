a7fd8b86600b936f91d5c53ab6e3e79cce2af37d
John Gamboa <jhgamboa@ucsd.edu>
2015-02-04 17:15:13 -0800

    Added partials for homepage

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,38 @@
+'use strict'
+
 var app = angular.module("app", ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state("login", {
+            abstract: 'true',
             url: '/',
             templateUrl: "partials/login.html"
         })
+        .state("login.login", {
+            url: '',
+            templateUrl: "partials/login.login.html"
+        })
+        .state("login.register", {
+            url: 'register',
+            templateUrl: "partials/login.register.html"
+        })
         .state("home", {
+            abstract: true,
             url: '/home',
             templateUrl: "partials/home.html"
+        })
+        .state("home.groups", {
+            url: '',
+            templateUrl: "partials/home.groups.html"
+        })
+        .state("home.buddies", {
+            url: '/buddies',
+            templateUrl: "partials/home.buddies.html"
+        })
+        .state("home.profile", {
+            url: '/profile',
+            templateUrl: "partials/home.profile.html"
         });
     $urlRouterProvider.otherwise("/");
 });
