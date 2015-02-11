db15066711e358ee23394808cf02013f81012c37
Ray Yada <rayada@ucsd.edu>
2015-02-10 17:47:39 -0800

    poop

--- static/js/studybuddy.js
+++ static/js/studybuddy.js
@@ -2,7 +2,7 @@
 
 var app = angular.module("app", ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
         .state("login", {
             abstract: 'true',
@@ -17,6 +17,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
             url: 'register',
             templateUrl: "partials/login.register.html"
         })
+        .state("login.about", {
+            url: 'about',
+            templateUrl: "partials/login.about.html"
+        })
         .state("home", {
             abstract: true,
             url: '/home',
@@ -25,6 +29,11 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state("home.groups", {
             url: '',
             templateUrl: "partials/home.groups.html"
+        })
+        .state("home.group", {
+            url: '/:class/:owner',
+            templateUrl: "partials/home.group.html",
+            controller: "GroupCtrl"
         })
         .state("home.buddies", {
             url: '/buddies',
@@ -37,6 +46,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
         .state("home.add", {
             url: '/add',
             templateUrl: "partials/home.add.html"
+        })
+        .state("home.add.create", {
+            url: '/create',
+            templateUrl: "partials/home.add.create.html"
+        })
+        .state("home.add.join", {
+            url: '/join',
+            templateUrl: "partials/home.add.join.html",
+            controller: "GroupsCtrl"
+        })
+        .state("home.buddy", {
+            url: '/:username',
+            templateUrl: "partials/home.buddy.html",
+            controller: 'BuddyCtrl'
         });
     $urlRouterProvider.otherwise("/");
 });
@@ -44,3 +67,86 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.config(function($locationProvider) {
     $locationProvider.html5Mode(true);
 });
+
+app.controller('UserCtrl', function($http, $scope) {
+    $http.get("data/user.json").success(function(data) {
+        $scope.user = data;
+    });
+});
+
+app.controller('UserGroupsCtrl', function($scope, $http) {
+    $http.get("data/usergroups.json").success(function(data) {
+        $scope.groups = data;
+    });
+});
+
+
+app.controller('GroupsCtrl', function($scope, $http) {
+    $http.get("data/groups.json").success(function(data) {
+        $scope.groups = data;
+    });
+});
+
+app.controller('GroupCtrl', function($scope, $http, $state, $stateParams) {
+
+    $scope.class = $stateParams.class;
+    $scope.owner = $stateParams.owner;
+
+    $http.get("data/groups.json").success(function(data) {
+        $scope.groups = data;
+        $scope.group = getGroup($scope.groups, $scope.class, $scope.owner);
+    });
+
+    function getGroup(data, subject, owner) {
+        for(var i = 0; i < data.length; i += 1) {
+            if(data[i].class === subject && data[i].owner === owner) {
+                return data[i];
+            }
+        }
+        return 0;
+    }
+});
+
+app.controller('BuddiesCtrl', function($http, $scope) {
+    $http.get("data/buddies.json").success(function(data) {
+        $scope.buddies = data;
+    });
+});
+
+app.controller('BuddyCtrl', function($http, $scope, $state, $stateParams) {
+
+    $scope.name = $stateParams.username;
+
+    $http.get("data/buddies.json").success(function(data) {
+        $scope.buddies = data;
+        $scope.buddy = getByUsername($scope.buddies, $scope.name);
+    });
+
+    function getByUsername(data, name) {
+        for(var i = 0; i < data.length; i += 1) {
+            if(data[i].username === name) {
+                return data[i];
+            }
+        }
+        return 0;
+    }
+});
+
+app.directive('ngConfirmClick', [
+        function(){
+            return {
+                link: function (scope, element, attr) {
+                    var msg = attr.ngConfirmClick || "Are you sure?";
+                    var clickAction = attr.confirmedClick;
+                    element.bind('click',function (event) {
+                        if ( window.confirm(msg) ) {
+                            scope.$eval(clickAction)
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    });
+                }
+            };
+    }])
